1628c34f179c7d9fcd98f61292061c30d9495a22
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-16 13:17:07 -0800

    task: search input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SearchBarComponent, PageListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ngx-wiki';
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   imports: [],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {
-
+export class SearchBarComponent implements OnInit {
+  term = '';
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.term = value;
+  }
+  onFormSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.term);
+  }
+  ngOnInit(): void {}
 }
